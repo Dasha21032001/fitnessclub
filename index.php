51038b2e7c67e5500c6ec761122187d73b4993d8
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-02-23 13:17:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device=width, iniyial-
+    scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="img/icon.png">
+    <title>Фитнес-клуб</title>  
+</head>
+<header>
+   <div class="header">
+       <div class="header_section">
+           <div class="header_item headerlogo">
+               Фитнес-клуб
+           </div>
+           <div class="header_item headerButton"><a href="#">Услуги</a></div>
+           <div class="header_item headerButton"><a href="#">Тренеры</a></div>
+           <div class="header_item headerButton"><a href="#">Расписание</a></div>
+       </div>
+       <div class="header_section">
+       <div class="header_item headerButton"><a href="#">Войти</a></div>
+       <div class="header_item headerButton"><a href="#">Зарегистрироваться</a></div>
+       </div>
+   </div>
+</header>
+
+<body> 
+
+</body>
+
+<footer>
+
+</footer>
+                                                          
+</html>
