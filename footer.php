60f835ff94f62282b9a7bd9bde895221b416e3f6
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-02-28 16:51:43 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+<div><center>Все права защищены &copy <?php echo date ('Y')?></center></div>
+<br/>
+
+<div class="box-image"><center>
+<a href="https://vk.com/hhhhhlebushek" target="_blank"><img src="img/vk.png" /></a>
+
+<a href="https://www.instagram.com/hhhlebushek" target="_blank"><img src="img/instagram.png" /></a>
+<center></div>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,19 @@
+<header>
+   <div class="header">
+       <div class="header_section">
+           <div class="header_item headerlogo">
+               Фитнес-клуб
+           </div>
+           <div class="header_item headerButton"><a href="services.php">Услуги</a></div>
+           <div class="header_item headerButton"><a href="coaches.php">Тренеры</a></div>
+           <div class="header_item headerButton"><a href="schedule.php">Расписание</a></div>
+       </div>
+       <div class="header_section">
+       <div class="header_item headerButton"><a href="#">Войти</a></div>
+       <div class="header_item headerButton"><a href="#">Зарегистрироваться</a></div>
+       </div>
+   </div>
+</header>
+
+
+
