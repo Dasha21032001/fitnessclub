e33a46cd6ac0d1c1269e7b2b9fc9c24ce9d109df
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-03-07 18:26:51 +0300

    Update header.php

--- header.php
+++ header.php
@@ -2,15 +2,15 @@
    <div class="header">
        <div class="header_section">
            <div class="header_item headerlogo">
-               Фитнес-клуб
+           <a href="index.php">Фитнес-клуб</a>
            </div>
            <div class="header_item headerButton"><a href="services.php">Услуги</a></div>
            <div class="header_item headerButton"><a href="coaches.php">Тренеры</a></div>
            <div class="header_item headerButton"><a href="schedule.php">Расписание</a></div>
        </div>
        <div class="header_section">
-       <div class="header_item headerButton"><a href="#">Войти</a></div>
-       <div class="header_item headerButton"><a href="#">Зарегистрироваться</a></div>
+       <div class="header_item headerButton"><a href="avto.php">Войти</a></div>
+       <div class="header_item headerButton"><a href="register.php">Зарегистрироваться</a></div>
        </div>
    </div>
 </header>
