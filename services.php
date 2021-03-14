4a34bc825b72937479f56848a22c59e26bf130d3
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-03-14 21:48:44 +0300

    Update services.php

--- services.php
+++ services.php
@@ -1,3 +1,34 @@
-<div>
-тут будут услуги :)
-</div> 
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device=width, iniyial-
+    scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="img/icon.png">
+    <title>Фитнес-клуб.Услуги</title>  
+</head>
+
+
+<body> 
+    
+    <?php require "header.php"?>
+    <div class="content">
+    <?php
+    if($_COOKIE['user'] == ''):
+    ?> 
+        ТО, ЧТО ВИДИТ ОБЫЧНЫЙ ПОЛЬЗОВАТЕЛЬ в услугах :))))));
+<?php else: ?>                                      
+    <?php require "helloy.php"?>
+<?php endif; ?>
+</div>
+<?php require "footer.php"?>
+
+</body>
+
+
+
+                                                          
+</html>
