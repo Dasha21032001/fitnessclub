88ef17d0a95b0e83d4bd06d54ceec842408aed8d
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-03-25 16:35:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,33 @@
     <?php require "header.php"?>
     <div class="content">
     <?php
-    if($_COOKIE['user'] == ''):
+    session_start();
+    
+    if(empty($_SESSION["klient_id"])):
     ?> 
-        ТО, ЧТО ВИДИТ ОБЫЧНЫЙ ПОЛЬЗОВАТЕЛЬ :))))));
-<?php else: ?>                                      
-    <?php require "helloy.php"?>
-<?php endif; ?>
+        ТО, ЧТО ВИДИТ ОБЫЧНЫЙ ПОЛЬЗОВАТЕЛЬ на главной странице :))))));
+        
+    <?php else: ?>                                      
+        <?php require "helloy.php"?>
+
+        <?php
+            if(empty($_SESSION["klient_id"])){
+                echo("Не залогинен");
+            }
+            else if(($_SESSION["employee"]) && ($_SESSION["code_d"] == 1)){
+                echo("Админ привет ");
+            }
+            else if($_SESSION["employee"]){
+                echo("Сотрудник привет ");
+            }
+            else{
+                echo("Клиент привет");
+            }
+            //echo("code:".$_SESSION["code_d"]);
+            //echo("CID:".!empty($_SESSION["code_d"]).":".$_SESSION["code_d"]);
+            ?> 
+    
+    <?php endif; ?>
 </div>
 <?php require "footer.php"?>
 
