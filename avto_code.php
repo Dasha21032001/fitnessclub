2e382725cb4848bd55dc178c0a7191747ba3adc1
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-03-25 16:20:28 +0300

    Update avto_code.php

--- avto_code.php
+++ avto_code.php
@@ -5,17 +5,8 @@
     
     $password = md5($password."kailnicshaeava");
 
-    $servername = "localhost";
-    $database = "фитнес-клуб";
-    $username = "root";
-    $pass = "alisa1983";
-
-    // Создаем соединение
-    $conn = mysqli_connect($servername, $username, $pass, $database);
+    require "connect_db.php";
     
-
-    
-
     $query1 = "SELECT * FROM `список клиентов` WHERE `Логин` = '$login' AND  `Пароль` = '$password'";
     $query2 = "SELECT * FROM `сотрудники` WHERE `Логин` = '$login' AND  `Пароль` = '$password'";
 
@@ -24,13 +15,20 @@
 
     $result2=mysqli_query($conn,$query2);
     $user2=mysqli_fetch_assoc($result2);
-
+    session_start();
     if(count($user1) != 0 ){
-        setcookie('user', $user1['Имя'], time() + 3600, "/");
+        $_SESSION["klient_id"]=$user1['id_Клиента'];
+        $_SESSION["name"]=$user1['Имя'];
+        $_SESSION["employee"]=false;
+        //setcookie('user', $user1['Имя'], time() + 3600, "/");
         header('Location: index.php');
     }
     else if(count($user2) != 0){
-        setcookie('user', $user2['Имя'], time() + 3600, "/");
+        $_SESSION["klient_id"]=$user2['id_Сотрудника'];
+        $_SESSION["name"]=$user2['Имя'];
+        $_SESSION["employee"]=true;
+        $_SESSION["code_d"]=$user2['Должности_Код должности'];
+        //setcookie('user', $user2['Имя'], time() + 3600, "/");
         header('Location: index.php');
     }
     else{
