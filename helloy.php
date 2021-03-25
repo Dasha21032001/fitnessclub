83d1c5d9d222d7e7e3ef66eb9324799cadb3e27d
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-03-25 16:35:07 +0300

    Update helloy.php

--- helloy.php
+++ helloy.php
@@ -1,3 +1,3 @@
 
-<div class="helloy"><p align="right" >Здравствуйте, <?=$_COOKIE['user']?>. Чтобы выйти нажмите <a class="link-output" href="exit.php" ><font size="4" style="color: rgb(214, 12, 12);">здесь</font></a>.</p></div>
+<div class="helloy"><p align="right" >Здравствуйте, <?=$_SESSION["name"]?>. Чтобы выйти нажмите <a class="link-output" href="exit.php" ><font size="4" style="color: rgb(214, 12, 12);">здесь</font></a>.</p></div>
 
