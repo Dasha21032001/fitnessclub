5efa7565f0cfc33b3e89c8bb8acdd5ac3679249a
hhhhhlebushek <55104794+Dasha21032001@users.noreply.github.com>
2021-03-25 16:40:40 +0300

    Update register.php

--- register.php
+++ register.php
@@ -16,7 +16,7 @@
 <body> 
 <center>
 <h3>Регистрация</h3>
-<form action="code.php" method="post">
+
 <form action="register_code.php" method="post">
 <input class="register_important" type="text" name="surname" id="surname" placeholder="Введите фамилию"><br>
 <input class="register_important" type="text" name="name" id="name" placeholder="Введите имя"><br>
@@ -27,13 +27,10 @@
 <button type="submit">Зарегистрироваться</button>
 <br></br>
 
+<a class="link-output" href="register_sotr.php" ><font size="3" style="color: rgb(214, 12, 12);">Вы сотрудник?</font></a>
 
-<p class="forget_password" ><font size="3" style="color: rgb(214, 12, 12);">Сотрудник:</font></p>
-<input class="register_important" type="text" name="name_s" id="name_s" placeholder="Введите кодовое слово"><br>
-<button type="submit">Отправить</button>
-</form>
 
-</form>
+
 </center>
 </body>
 
